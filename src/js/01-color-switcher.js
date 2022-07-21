ac5bd22cc5f6b8a505892ec2a094befe18330895
JuliaOraz <uliaoraz@gmail.com>
2022-07-21 14:40:30 +0300

    add color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let bodyColor = null;
 
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', onStartBtn)
+stopBtn.addEventListener('click', onStopBtn)
+
+
+function onStartBtn() { 
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    bodyColor = setInterval(() => { 
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)  
+}
+
+function onStopBtn() { 
+    clearInterval(bodyColor);
+    if (startBtn.disabled) { 
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+    } 
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
