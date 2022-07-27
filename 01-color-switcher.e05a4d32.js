a91db2daa16a8936a4b5ac94f2c5b32a09fb02e6
JuliaOraz <97658697+JuliaOraz@users.noreply.github.com>
2022-07-27 09:21:48 +0000

    Deploying to gh-pages from @ JuliaOraz/goit-js-hw-09@21440d596c1f4d1fed971aa35533df6a9864fa38 🚀

---
+++ 01-color-switcher.e05a4d32.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled&&(t.disabled=!1,e.disabled=!0)}))}();
+//# sourceMappingURL=01-color-switcher.e05a4d32.js.map
